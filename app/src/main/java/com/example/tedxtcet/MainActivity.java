6f13476009acfa39b2574f996f1db18b1a39ff27
shivamg1404 <shivamg1404@gmai.com>
2024-03-19 21:46:12 +0530

    First Commit

---
+++ app/src/main/java/com/example/tedxtcet/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.tedxtcet;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://www.tedxtcet.com/");
+    }
+}
